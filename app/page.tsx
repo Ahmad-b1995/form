594bce9d545e441ed19129977720122adeb49591
Ahmad <ahmad.b1995@gmail.com>
2025-05-15 16:03:12 +0330

    minor

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import { RegistrationForm } from "@/components/registration-form";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gradient-to-b from-background via-background to-background/50 flex flex-col items-center justify-center p-4 sm:p-6 md:p-8">
-      <div className="w-full p-4 md:max-w-[30%] max-w-[440px] bg-card/30 backdrop-blur-sm rounded-lg shadow-2xl overflow-hidden border border-border/10">
+    <main className="min-h-screen bg-gradient-to-b from-background via-background to-background/50 flex flex-col items-center justify-center px-2 md:p-8">
+      <div className="w-full p-4 max-w-[440px] md:max-w-[60%] lg:max-w-[30%]  bg-card/30 backdrop-blur-sm rounded-lg shadow-2xl overflow-hidden border border-border/10">
           <div className="space-y-1 sm:space-y-2 mb-4 sm:mb-6 ">
             <h1 className="text-xl sm:text-2xl font-bold tracking-tight">FORMATION</h1>
             <p className="text-sm sm:text-base text-muted-foreground font-bold">
